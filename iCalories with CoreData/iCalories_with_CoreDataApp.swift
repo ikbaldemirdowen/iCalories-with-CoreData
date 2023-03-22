c706cf241436227d19e459a6f02bdb5d8a7bb122
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-03-21 23:51:15 -0700

    Initial Commit

---
+++ iCalories with CoreData/iCalories_with_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCalories_with_CoreDataApp.swift
+//  iCalories with CoreData
+//
+//  Created by Ikbal Demirdoven on 2023-03-21.
+//
+
+import SwiftUI
+
+@main
+struct iCalories_with_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
